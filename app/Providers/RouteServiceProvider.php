ad58666dc5f057f079bc1d2b2b0ce5f6bbdb7bf0
carlos <carlos.20499@gmail.com>
2020-05-27 23:59:03 -0400

    creando los componentes iniciales

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -72,8 +72,8 @@ class RouteServiceProvider extends ServiceProvider
      */
     protected function mapApiRoutes()
     {
-        Route::prefix('api')
-            ->middleware('api')
+        //apirestful.test/
+        Route::middleware('api')
             ->namespace($this->namespace)
             ->group(base_path('routes/api.php'));
     }
